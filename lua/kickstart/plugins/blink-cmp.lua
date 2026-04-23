366625f337451ee71043e90c4752cc63f3aa6c58
Peter Dalgaard-Jensen <pedal@eg.dk>
2026-04-23 15:33:38 +0200

    Adding c# debugger and fixing msall issues in blinkcmp

--- lua/kickstart/plugins/blink-cmp.lua
+++ lua/kickstart/plugins/blink-cmp.lua
@@ -21,12 +21,12 @@ return {
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
         opts = {},
       },
@@ -37,126 +37,104 @@ return {
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-      keymap = {
-        -- 'default' (recommended) for mappings similar to built-in completions
-        --   <c-y> to accept ([y]es) the completion.
-        --    This will auto-import if your LSP supports it.
-        --    This will expand snippets if the LSP sent a snippet.
-        -- 'super-tab' for tab to accept
-        -- 'enter' for enter to accept
-        -- 'none' for no mappings
-        --
-        -- For an understanding of why the 'default' preset is recommended,
-        -- you will need to read `:help ins-completion`
-        --
-        -- No, but seriously. Please read `:help ins-completion`, it is really good!
-        --
-        -- All presets have the following mappings:
-        -- <tab>/<s-tab>: move to right/left of your snippet expansion
-        -- <c-space>: Open menu or open docs if already open
-        -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-        -- <c-e>: Hide menu
-        -- <c-k>: Toggle signature help
-        --
-        -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'default',
 
-        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+      appearance = {
+        use_nvim_cmp_as_default = false,
+        nerd_font_variant = 'normal',
+      },
+      completion = {
+        accept = { auto_brackets = { enabled = true } },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 250,
+          update_delay_ms = 50,
+          treesitter_highlighting = true,
+          window = { border = 'rounded' },
+        },
+        list = {
+          selection = {
+            preselect = false,
+            auto_insert = false,
+          },
+        },
+        menu = {
+          border = 'rounded',
+          draw = {
+            columns = {
+              { 'label', 'label_description', gap = 1 },
+              { 'kind_icon', 'kind' },
+            },
+            treesitter = { 'lsp' },
+            kind_icon = {
+            text = function(ctx)
+              return require('lspkind').symbol_map[ctx.kind] or ''
+            end,
+            },
+          },
+        },
       },
 
-    appearance = {
-      use_nvim_cmp_as_default = false,
-      nerd_font_variant = 'normal',
-    },
-    completion = {
-      accept = { auto_brackets = { enabled = true } },
-      documentation = {
-        auto_show = true,
-        auto_show_delay_ms = 250,
-        update_delay_ms = 50,
-        treesitter_highlighting = true,
+      -- My super-TAB configuration
+      keymap = {
+        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        ['<C-e>'] = { 'hide', 'fallback' },
+        ['<CR>'] = { 'accept', 'fallback' },
+
+        ['<Tab>'] = {
+          function(cmp)
+            return cmp.select_next()
+          end,
+          'snippet_forward',
+          'fallback',
+        },
+        ['<S-Tab>'] = {
+          function(cmp)
+            return cmp.select_prev()
+          end,
+          'snippet_backward',
+          'fallback',
+        },
+
+        ['<Up>'] = { 'select_prev', 'fallback' },
+        ['<Down>'] = { 'select_next', 'fallback' },
+        ['<C-p>'] = { 'select_prev', 'fallback' },
+        ['<C-n>'] = { 'select_next', 'fallback' },
+        ['<C-up>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-down>'] = { 'scroll_documentation_down', 'fallback' },
+      },
+
+      -- Experimental signature help support
+      signature = {
+        enabled = true,
         window = { border = 'rounded' },
       },
-      list = {
-        selection = {
-          preselect = false,
-          auto_insert = false,
-        },
-      },
-      menu = {
-        border = 'rounded',
-        draw = {
-          columns = {
-            { 'label', 'label_description', gap = 1 },
-            { 'kind_icon', 'kind' },
+
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
+        providers = {
+          lsp = {
+            min_keyword_length = 2, -- Number of characters to trigger porvider
+            score_offset = 0, -- Boost/penalize the score of the items
           },
-          treesitter = { 'lsp' },
-        },
-      },
-    },
-
-    -- My super-TAB configuration
-    keymap = {
-      ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-      ['<C-e>'] = { 'hide', 'fallback' },
-      ['<CR>'] = { 'accept', 'fallback' },
-
-      ['<Tab>'] = {
-        function(cmp)
-          return cmp.select_next()
-        end,
-        'snippet_forward',
-        'fallback',
-      },
-      ['<S-Tab>'] = {
-        function(cmp)
-          return cmp.select_prev()
-        end,
-        'snippet_backward',
-        'fallback',
-      },
-
-      ['<Up>'] = { 'select_prev', 'fallback' },
-      ['<Down>'] = { 'select_next', 'fallback' },
-      ['<C-p>'] = { 'select_prev', 'fallback' },
-      ['<C-n>'] = { 'select_next', 'fallback' },
-      ['<C-up>'] = { 'scroll_documentation_up', 'fallback' },
-      ['<C-down>'] = { 'scroll_documentation_down', 'fallback' },
-    },
-
-    -- Experimental signature help support
-    signature = {
-      enabled = true,
-      window = { border = 'rounded' },
-    },
-
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
-      providers = {
-        lsp = {
-          min_keyword_length = 2, -- Number of characters to trigger porvider
-          score_offset = 0, -- Boost/penalize the score of the items
-        },
-        path = {
-          min_keyword_length = 0,
-        },
-        snippets = {
-          min_keyword_length = 2,
-        },
-        buffer = {
-          min_keyword_length = 5,
-          max_items = 5,
-        },
-        copilot = {
-          name = 'copilot',
-          module = 'blink-cmp-copilot',
-          score_offset = 0,
-          async = true,
+          path = {
+            min_keyword_length = 0,
+          },
+          snippets = {
+            min_keyword_length = 2,
+          },
+          buffer = {
+            min_keyword_length = 5,
+            max_items = 5,
+          },
+          copilot = {
+            name = 'copilot',
+            module = 'blink-cmp-copilot',
+            score_offset = 0,
+            async = true,
+          },
         },
       },
     },
   },
-},
 }
 -- vim: ts=2 sts=2 sw=2 et
